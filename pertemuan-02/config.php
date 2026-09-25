2d601aaccca8995d0bd0a4e3274c94296e5204df
Salsabilla <2511500083@mahasiswa.atmaluhur.ac.id>
2026-09-25 18:04:05 +0700

    Pertemuan-02

---
+++ pertemuan-02/config.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . "/helpers/response.php";
+
+mysqli_report(MYSQLI_REPORT_OFF);
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "db_akademik";
+
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+
+if (!$koneksi) {
+    sendResponse(
+        false,
+        "Koneksi database gagal",
+        null,
+        500
+    );
+}
+
+?>
